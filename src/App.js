9bd6e6c3aea4b46aef2d4f563770bdbabe96c547
Shlok Verma <87667355+SoraUchiha@users.noreply.github.com>
2023-05-06 19:47:58 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,30 @@
+
+import './App.css';
+import {
+  BrowserRouter as Router,
+  Routes,
+  Route,
+  Link
+} from "react-router-dom";
+import Home from './pages/home/Home'
+import List from './pages/list/List';
+import Hotel from './pages/hotel/Hotel';
+
+function App() {
+  return (
+    <Router>
+       <Routes>
+          <Route path='/' element={<Home/>}/>
+          <Route path='/hotels' element={<List/>}/>
+          <Route path='/hotels/:id' element={<Hotel/>}/>
+{/* 18;23 */}
+
+
+       </Routes>
+    
+    
+    </Router>
+  );
+}
+
+export default App;
